bea32e763d5ce071323fcf4a7c1029dd5e4f288a
Elcan <eljanng@code.edu.az>
2023-02-07 02:28:55 +0400

    Js intro practice

---
+++ script.js
@@ -0,0 +1,160 @@
+
+// console.log("hello")
+
+// alert("salam Cavid")
+
+// "use strict"
+
+
+// var surname = "Ismayilzade";
+// var name = "Cavid";
+
+// console.log(surname + " " + name);
+
+
+
+// let name = "Cavid";
+
+// const IS_MARRIED = true
+
+
+
+// if(true){
+//     let age = 55; 
+//     console.log(age)
+// }
+
+
+
+
+let age = 17;
+
+let name = "Cavid";
+
+let isMarried = false;
+
+let address;
+
+let phone = null;
+
+// console.log(typeof age)
+
+// console.log(typeof name)
+
+// console.log(typeof isMarried)
+
+// console.log(typeof address)
+
+// console.log(typeof phone)
+
+
+// if(phone == null){
+//     console.log("Address null oldu")
+// }
+
+// let stu = {
+//     name: "Cavid",
+//     surname: "Ismayilzade"
+// }
+
+// // console.log(stu.name + " " + stu.surname)
+// console.log( `${stu.name} ${stu.surname}`);
+
+
+
+// let students = ["Cavid", "Pervin", "Mirze", 100,200, true];
+
+// // console.log("Cavid/3")
+
+// for(let i = 0; i < students.length; i++){
+//     console.log(students[i])
+// }
+
+
+// alert("salamlar")
+
+
+// let username = prompt("Please add your username")
+
+// console.log(username)
+
+
+// let isOnline = confirm("Is he online ?")
+
+// debugger
+
+// console.log(isOnline)
+
+// if(isOnline){
+//     console.log("He is online")
+// }else{
+//     console.log("He is offline")
+// }
+
+
+
+
+// let age = 20;
+
+// if(age > 15){
+//     console.log("boyukdur")
+// }else{
+//     console.log("kicikdir")
+// }
+
+
+// let temp = 25;
+
+// if(temp < 10){
+//     console.log("soyuqdur")
+// }else if (temp > 20) {
+//     console.log("istidir")
+// }else{
+//     console.log("hecbisey")
+// }
+
+
+// let count = 6;
+
+// switch (count) {
+//     case 5:
+//         console.log("bes")
+//         break;
+//     case 6:
+//         console.log("alti")
+//     default:
+//         console.log("notfound")
+// }
+
+
+// for(let i = 0; i<20; i++){
+//     console.log(i)
+// }
+
+
+// let result = 2**3;
+
+// console.log("44c"*2)
+
+
+// let IS_MARRIED = true;
+
+// let result = IS_MARRIED?"Evlidir":"Subaydir";
+
+// console.log(result)
+
+let addresss = "";
+
+let result = addresss??"Ehmedli";
+
+console.log(result);
+
+if(addresss == null){
+    console.log("Ehmedli")
+}else{
+    console.log(addresss)
+}
+
+let res = address == null ? "Ehmedli":address;
+
+console.log(res)
